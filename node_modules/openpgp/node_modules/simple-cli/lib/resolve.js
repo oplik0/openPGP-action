dbeb13717da385c470a6f0f42bf168d1add0f825
Opliko <opliko.reg@protonmail.com>
2021-12-12 12:16:34 +0000

    chore: update dependencies properly

--- node_modules/openpgp/node_modules/simple-cli/lib/resolve.js
+++
@@ -1,1 +0,0 @@
-module.exports = (path) => require.resolve(path);
--- node_modules/openpgp/src/config/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * @fileoverview This object contains global configuration values.
- * @see module:config/config
- * @module config
- */
-
-export { default } from './config.js';
